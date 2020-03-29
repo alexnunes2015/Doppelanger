59073025aa4d05d17bcb603f8a6546b9dc3735d2
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-03-29 22:51:02 +0100

    [ADD] Add simple sound system to menu

---
+++ scripts/menu.js
@@ -0,0 +1,21 @@
+const sndClick = new Audio('./sounds/click.mp3');
+
+function playClickSound(){
+    sndClick.play();
+}
+
+function newGame(){
+    playClickSound();
+}
+
+function continueGame(){
+    playClickSound();
+}
+
+function quit(){
+    playClickSound();
+}
+
+function credits(){
+    playClickSound();
+}
